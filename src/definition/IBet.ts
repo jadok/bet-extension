267efb1039f2afed548ba28883e2175fc615beb9
jadok <quentin.jadeau@live.fr>
2019-10-02 13:42:36 +0200

    fix: folder structure

---
+++ src/definition/IBet.ts
@@ -0,0 +1,15 @@
+export interface IBet {
+  odd: number;
+  status: string;
+
+  /**
+   *
+   */
+  peoples: string;
+  /**
+   * Sport name which was bet on.
+   */
+  sport: string;
+
+  date: string;
+}
